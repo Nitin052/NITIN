f1e81293c4601941fd4543f20269f3bfc590adc9
Nitin052 <42588508+Nitin052@users.noreply.github.com>
2019-04-06 15:27:44 +0530

    Update os.c

--- os.c
+++ os.c
@@ -5,4 +5,20 @@ int main()
 	int queue[5000],n,c,head,diff,dist;
 	printf("Enter the size of queue= ");
 	scanf("%d",&n);
+	printf("Enter the element in the queue=");
+	for (int i=0;i<n;i++)
+	{
+	scanf("%d",&queue[n]);
+    }
+	printf("Enter the head position=");
+	scanf("%d",&c);
+	queue[0]=head;
+	for(int j=0;j<n;j++)
+	{
+		diff=abs(queue[j+1]-queue[j]);
+		dist=dist+diff;
+	}
+	printf("Total distance=%d",dist);
+	
+}
 
